04cbb0589acbc0fa6b49a2e0717402d591b4035b
William <william.walshdowd@gmail.com>
2022-04-04 18:41:49 +0100

    added required functions to algorithm calculator

--- Final Project/AlgorithmCalculator.java
+++ Final Project/AlgorithmCalculator.java
@@ -1,8 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class AlgorithmCalculator {
+	FileReader reader;
+	BufferedReader buffer;
 	
-	AlgorithmCalculator(String fileName) {
+	AlgorithmCalculator() {
 		
+	}
+	
+	// Functions that perform the 3 required tasks
+	//
+	public static String fastestRouteBetween(String stop1, String stop2) {
+		//TODO write function
+	}
+	
+	public static String searchForStopByName(String name) {
+		//TODO write function
+	}
+	
+	public static String searchForStopByTime(String time) {
+		//TODO write function
+	}
+	
+	
+	// Functions to analyse and load in data from .txt files
+	//
+	private static void loadFiles() {
+		
+	}
+	
+	private static void loadStopTimesFile() {
+		
+	}
+	
+	private static void loadStopsFile() {
+		
+	}
+
+	private static void loadTransfersFile() {
+	
 	}
 
 }
